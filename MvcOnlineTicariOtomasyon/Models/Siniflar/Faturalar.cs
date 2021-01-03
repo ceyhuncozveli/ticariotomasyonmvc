b71e4ef79a192986069286627ca5f4d8bcbac73a
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-01-03 07:12:42 +0300

    Add new classes

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Faturalar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Faturalar
+    {
+        [Key]
+        public int FaturaId { get; set; }
+        public char FaturaSeriNo { get; set; }
+        public string FaturaSiraNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public string VergiDairesi { get; set; }
+        public DateTime Saat { get; set; }
+        public string TeslimEden { get; set; }
+        public string TeslimAlan { get; set; }
+    }
+}
