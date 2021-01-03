2f43c40e340caab22fe17743bf5905760c16578e
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-01-03 06:54:23 +0300

    Add cari and personel classes

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Urun.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
 {
     public class Urun
     {
+        [Key]
         public int UrunID { get; set; }
         public string UrunAd { get; set; }
         public string Marka { get; set; }
@@ -14,5 +16,6 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public decimal AlisFiyat { get; set; }
         public decimal SatisFiyat { get; set; }
         public bool Durum { get; set; }
+        public string UrunGorsel { get; set; }
     }
 }
