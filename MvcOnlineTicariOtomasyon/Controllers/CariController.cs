7608a5a8a1849cb64c652c5b6d2ff455a700d73a
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-06 16:42:56 +0300

    Add current page view
    Cariler

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        // GET: Cari
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var cariler = context.Carilers.ToList();
+            return View(cariler);
+        }
+    }
+}
