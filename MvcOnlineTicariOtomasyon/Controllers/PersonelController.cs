4536e81daf290b8e0a853b0e63b2c213ff06f6bc
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-07 20:03:21 +0300

    Add personel insert operation

--- MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -17,5 +17,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var personeller = context.Personels.ToList();
             return View(personeller);
         }
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> departmanList = (from x in context.Departmans.ToList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.DepartmanAd, // Kullaniciya dropdownda gosterilecek deger
+                                                     Value = x.DepartmanId.ToString() // Arka planda secilen itemin istenilen degeri, id ile islem yapilacagi icin id degerini burada value olarak aliyoruz
+                                                 }).ToList();
+
+            ViewBag.departmanlar = departmanList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel personel)
+        {
+            context.Personels.Add(personel);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
