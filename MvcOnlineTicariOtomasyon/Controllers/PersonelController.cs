220c1b371e38686ca11e0268faccb1e2e6155fc0
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-07 19:49:29 +0300

    Add personel view page

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var personeller = context.Personels.ToList();
+            return View(personeller);
+        }
+    }
+}
