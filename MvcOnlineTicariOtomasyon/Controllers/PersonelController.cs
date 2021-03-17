dc17c175e45d2ef42b9e91946e5ad11636559b61
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-17 23:05:30 +0300

    Add personel update operation

--- MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -40,5 +40,31 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> departmanList = (from x in context.Departmans.ToList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.DepartmanAd, // Kullaniciya dropdownda gosterilecek deger
+                                                      Value = x.DepartmanId.ToString() // Arka planda secilen itemin istenilen degeri, id ile islem yapilacagi icin id degerini burada value olarak aliyoruz
+                                                  }).ToList();
+
+            ViewBag.departmanlar = departmanList;
+
+            var personel = context.Personels.Find(id);
+            return View("PersonelGetir", personel);
+        }
+
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsnl = context.Personels.Find(p.PersonelID);
+            prsnl.PersonelAd = p.PersonelAd;
+            prsnl.PersonelSoyad = p.PersonelSoyad;
+            prsnl.PersonelGorsel = p.PersonelGorsel;
+            prsnl.DepartmanId = p.DepartmanId;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
