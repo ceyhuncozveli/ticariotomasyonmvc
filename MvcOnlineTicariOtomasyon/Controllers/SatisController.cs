d1f2d2e627039d61b35e06713d7b1319c7152c5d
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-21 00:04:56 +0300

    Add new sales operation

--- MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -20,12 +20,38 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            List<SelectListItem> deger1 = (from x in context.Uruns.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.UrunAd,
+                                             Value = x.UrunID.ToString()
+                                         }).ToList();
+
+            List<SelectListItem> deger2 = (from x in context.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in context.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+
+
             return View();
         }
 
         [HttpPost]
         public ActionResult YeniSatis(SatisHareket satis)
         {
+            satis.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
             context.SatisHarekets.Add(satis);
             context.SaveChanges();
             return RedirectToAction("Index");
