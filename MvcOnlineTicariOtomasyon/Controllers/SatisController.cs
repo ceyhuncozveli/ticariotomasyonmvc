82ea59977c7d59667a3579eee937f87c118a57de
Ceyhun ÇÖZVELİOĞLU <c.cozvelioglu@gmail.com>
2021-03-20 23:48:06 +0300

    Add salesmoves index

---
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var satislar = context.SatisHarekets.ToList();
+            return View(satislar);
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket satis)
+        {
+            context.SatisHarekets.Add(satis);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
